ed0e180bae6da38140d7aaf49aa3e235a94de94d
Dennis Kountouris <dennisgk2005@gmail.com>
2026-01-25 01:00:17 -0500

    remove

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,11 @@ import { PyodideH5Provider } from "./contexts/PyodideH5Context";
 import { HomePage } from "./pages/HomePage";
 import { AnalyzePage } from "./pages/AnalyzePage";
 import { EditPage } from "./pages/EditPage";
-import BabelPage from "./pages/BabelPage";
 
 const router = createBrowserRouter([
   { path: "/", Component: HomePage },
   { path: "/analyze", Component: AnalyzePage },
   { path: "/edit", Component: EditPage },
-  { path: "/babel", Component: BabelPage },
 ]);
 
 createRoot(document.getElementById("root")!).render(
